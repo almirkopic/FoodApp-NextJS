cf54fabd1916d3ad0b2e25a443289a1bf579270b
Almir Kopic <almirkopic1992@gmail.com>
2024-11-18 13:40:19 -0600

    v1

---
+++ app/meals/[mealsSlug]/page.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function MealsSlugPage() {
+  return <h1>Meals Slug Page</h1>;
+}
+
+export default MealsSlugPage;
---
+++ app/meals/share/page.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function mealsShare() {
+  return <h1>Meals Share!</h1>;
+}
+
+export default mealsShare;
